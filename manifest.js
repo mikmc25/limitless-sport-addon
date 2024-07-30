577dd3f1a8d21ce85a7abea1ff9f670221ddcbfd
mikmc25 <167842116+mikmc25@users.noreply.github.com>
2024-07-30 23:16:40 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,9 +1,9 @@
 module.exports={
     id: "org.community.homescreen",
     version: "1.0.1",
-    name: "HY USA sports addon",
-    logo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b88a0693-6b8d-4bdf-a393-e0630bd298e3/daqi2i6-7a253564-9cbb-4ef6-a78c-ed1d863ac131.png/v1/fill/w_1024,h_1004/first_custom_dallas_stars_logo_by_nhlconcepts_daqi2i6-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwNCIsInBhdGgiOiJcL2ZcL2I4OGEwNjkzLTZiOGQtNGJkZi1hMzkzLWUwNjMwYmQyOThlM1wvZGFxaTJpNi03YTI1MzU2NC05Y2JiLTRlZjYtYTc4Yy1lZDFkODYzYWMxMzEucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.96glOZJn0b-HtPag97N3tml8LzlXUY8Z7lvjDeaFnlQ",
-    description: "This addon brings sports from the USA.",
+    name: "HY Darknight",
+    logo: "https://raw.githubusercontent.com/mikmc25/limitless-sport-addon/usa/logo-color.png",
+    description: "Darknight sports from the USA.",
     types: ["movie", "series","tv","channel"],
     background: "https://mrwallpaper.com/images/hd/batman-the-dark-knight-rises-4k-jwuxwi1azm1ww3er.jpg",
     resources: ["movie","series","tv"],
